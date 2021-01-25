2966fbe858d78c9147c430210399c8c31a4f34de
Omid2121 <Omid_Ahangi@github.com>
2021-01-25 15:15:40 +0100

    Edabit

---
+++ Edabit_Answers/Edabit_Answers/Answers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Edabit_Answers
+{
+    /*
+    //Answers.
+
+     //Array of Multiples
+
+            public static int[] Multiples(int num, int length)
+        {
+            int[] multiples = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                multiples[i] = num * (i + 1);
+            }
+            return multiples;
+        }
+
+
+    //The Collatz Conjecture
+
+    static void Main(string[] args)
+        {
+            Console.WriteLine(Counter(10));
+        }
+
+        public static int Counter(int num)
+        {
+            int count = 0;
+            while (num > 1)
+            {
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                    count++;
+                }
+                else if (num % 2 != 0)
+                {
+                    num = num * 3 + 1;
+                    count++;
+                }
+            }
+            return count;
+        }
+
+    //
+
+     */
+
+}
---
+++ Edabit_Answers/Edabit_Answers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Edabit_Answers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Counter(10));
+        }
+
+        public static int Counter(int num)
+        {
+            int count = 0;
+            while (num > 1)
+            {
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                    count++;
+                }
+                else if (num % 2 != 0)
+                {
+                    num = num * 3 + 1;
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
